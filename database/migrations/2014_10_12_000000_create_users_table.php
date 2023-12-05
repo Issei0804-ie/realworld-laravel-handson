d015fc055f608010edc3569b2c07777b1a0750f7
issei miyara <issei_miyara@rebuild-inc.co.jp>
2023-12-05 23:41:43 +0900

    users.imageはnullableだったので nullable入れといた

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('bio');
-            $table->string('image');
+            $table->string('image')->nullable();
             //https://github.com/PHP-Open-Source-Saver/jwt-auth
             $table->rememberToken();
             $table->timestamps();
