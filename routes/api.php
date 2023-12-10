d5ccede6dbfd5a5b0edadb437ce1adf73f3705a3
issei miyara <issei_miyara@rebuild-inc.co.jp>
2023-12-11 00:08:20 +0900

    ルーティングを書いた

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name('api.')->group(function () {
+    Route::prefix('/users')->name('users.')->group(function () {
+        Route::prefix('/login')->name('login.')->group(function () {
+            Route::post('/', \App\Http\Controllers\Api\Users\Login\StoreController::class)->name('store');
+        });
+    });
 });
