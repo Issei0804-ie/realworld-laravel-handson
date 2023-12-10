b2c881273c44d0d0990b6ba2486c8bc9377cbc1f
issei miyara <issei_miyara@rebuild-inc.co.jp>
2023-12-11 00:07:44 +0900

    バリデーションエラー時に422を返して欲しいので、その実装をした親クラスを作成した

---
+++ app/Http/Requests/Api/APIFormRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class APIFormRequest extends FormRequest
+{
+    public function failedValidation(\Illuminate\Contracts\Validation\Validator $validator): void
+    {
+        $response = response()->json([
+            'errors' => $validator->errors()
+        ], 422);
+        throw new \Illuminate\Validation\ValidationException($validator, $response);
+    }
+}
