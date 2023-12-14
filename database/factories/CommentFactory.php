b5c1661633b41f031e14cdcd234d644f18be3bc0
issei miyara <issei_miyara@rebuild-inc.co.jp>
2023-12-14 23:42:50 +0900

    'commented_article_id' => User::factory(), を 'commented_article_id' => Article::factory(), にした

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Article;
 use App\Models\Comment;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -14,7 +15,7 @@ class CommentFactory extends Factory
     public function definition(): array
     {
         return [
-            'commented_article_id' => User::factory(),
+            'commented_article_id' => Article::factory(),
             'commented_user_id' => User::factory(),
             'body' => $this->faker->paragraph(),
             'created_at' => Carbon::now(),
