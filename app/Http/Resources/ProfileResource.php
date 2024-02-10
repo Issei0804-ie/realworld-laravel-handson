fa1634d0f0bde39d3fbddc5552eb8492daec265f
issei miyara <issei_miyara@rebuild-inc.co.jp>
2024-02-11 00:46:43 +0900

    [add]えいや

---
+++ app/Http/Resources/ProfileResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProfileResource extends JsonResource
+{
+    public $collects = User::class;
+    public function __construct($resource, private ?User $userAccessedProfile = null)
+    {
+        parent::__construct($resource);
+    }
+    public function toArray(Request $request): array
+    {
+        return [
+            'profile' => [
+                'username' => $this->username,
+                'bio' => $this->bio,
+                'image' => $this->image_s3_path,
+                'following' => $this->userAccessedProfile ? $this->isFollowing($this->userAccessedProfile): false,
+            ],
+        ];
+    }
+}
