02975b6cbd68b2eb7dbd258ffaaa51024f2cd6cf
issei miyara <issei_miyara@rebuild-inc.co.jp>
2024-02-11 14:26:41 +0900

    follow機能done

---
+++ app/Http/Controllers/Api/Profiles/Username/Follow/StoreController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\Profiles\Username\Follow;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProfileResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    public function __invoke(User $user, Request $request)
+    {
+        $userAccessedThatAPI = $request->user();
+        if ($userAccessedThatAPI->isFollowing($user)) {
+            $this->unfollow($userAccessedThatAPI, $user);
+        } else {
+            $this->follow($userAccessedThatAPI, $user);
+        }
+
+
+        return response()->json(ProfileResource::make($user, $userAccessedThatAPI));
+    }
+
+    private function follow(User $source, User $target): void
+    {
+        $source->followYou($target);
+    }
+
+    private function unfollow(User $source, User $target): void
+    {
+        $source->unfollowYou($target);
+    }
+
+}
---
+++ tests/Feature/Http/Controllers/Api/Profiles/Username/Follow/StoreController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api\Profiles\Username\Follow;
+
+use App\Models\Follow;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class StoreController extends TestCase
+{
+    use RefreshDatabase;
+    private User $source;
+    private User $target;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->source = User::factory()->create();
+        $this->target = User::factory()->create();
+    }
+
+
+    #[Test]
+    public function 既にフォローしている状態でAPIを叩くとフォローを解除することができる()
+    {
+        $this->source->followYou($this->target);
+        $this->assertDatabaseCount('follows', 1);
+        $this->assertDatabaseHas('follows', [
+            'follow_user_id' => $this->target->id,
+            'follower_user_id' => $this->source->id,
+        ]);
+
+        $this->actingAs($this->source)
+            ->post(route('api.profiles.username.follow.store', ['user' => $this->target]))
+            ->assertOk()
+            ->assertJson([
+                'profile' => [
+                    'username' => $this->target->username,
+                    'bio' => $this->target->bio,
+                    'image' => $this->target->image_s3_path,
+                    'following' => false,
+                ]
+            ]);
+
+        $this->assertDatabaseEmpty('follows');
+    }
+
+    #[Test]
+    public function フォローしていない状態でAPIを叩くとフォローすることができる()
+    {
+        $this->assertDatabaseEmpty('follows');
+
+        $this->actingAs($this->source)
+            ->post(route('api.profiles.username.follow.store', ['user' => $this->target]))
+            ->assertOk()
+            ->assertJson([
+                'profile' => [
+                    'username' => $this->target->username,
+                    'bio' => $this->target->bio,
+                    'image' => $this->target->image_s3_path,
+                    'following' => true,
+                ]
+            ]);
+
+        $this->assertDatabaseCount('follows', 1);
+        $this->assertDatabaseHas('follows', [
+            'follow_user_id' => $this->target->id,
+            'follower_user_id' => $this->source->id,
+        ]);
+    }
+}
